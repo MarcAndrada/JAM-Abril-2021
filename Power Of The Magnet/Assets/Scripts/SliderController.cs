0c3b76297cc21a6951dc1dce6532fc592aeec7be
jtomasenti <jordi.tomas@enti.cat>
2021-04-13 20:06:31 +0200

    Sound Manager y prueba de slider para sonido

---
+++ Power Of The Magnet/Assets/Scripts/SliderController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public Slider MusicSlider;
+    public Slider SFXSlider;
+
+    private SoundManager SFX_Cont;
+    private SoundManager Music_Cont;
+    private float Music = 0.1f;
+    private float SFX = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //BinaryReader reader;
+        //if (File.Exists("sound.sav"))
+        //{
+        //    reader = new BinaryReader(File.Open("sound.sav", FileMode.Open));
+        //    Music = reader.ReadSingle();
+        //    SFX = reader.ReadSingle();
+        //    reader.Close();
+        //}
+        //else { return; }
+
+        Music_Cont = GetComponentInChildren<SoundManager>();
+        SFX_Cont = GetComponent<SoundManager>();
+
+        if (MusicSlider != null)
+        {
+            MusicSlider = MusicSlider.GetComponent<Slider>();
+            MusicSlider.value = Music;
+        }
+        else
+        {
+            Music_Cont.SetVolume(Music);
+        }
+        if (SFXSlider != null)
+        {
+            SFXSlider = SFXSlider.GetComponent<Slider>();
+            SFXSlider.value = SFX;
+        }
+        else
+        {
+            SFX_Cont.SetVolume(SFX);
+        }
+    }
+
+    private void Update()
+    {
+        if (MusicSlider != null)
+        {
+            Music = MusicSlider.value;
+
+        }
+        if (SFXSlider != null)
+        {
+            SFX = SFXSlider.value;
+
+        }
+    }
+
+    // Update is called once per frame
+    public float GetMusicVol()
+    {
+        return Music;
+    }
+    public float GetSFXVol()
+    {
+        return SFX;
+    }
+
+
+    //public void SaveValues()
+    //{
+    //    BinaryWriter writer = new BinaryWriter(File.Open("sound.sav", FileMode.Create));
+    //    writer.Write(Music);
+    //    writer.Write(SFX);
+    //}
+}
---
+++ Power Of The Magnet/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    public static AudioClip Song, Caja, Iman;
+    static AudioSource audiosrc;
+
+    private float musicVolume;
+
+    Scene currentScene;
+    string sceneName;
+
+    private float introSongTimer;
+    private float gameSongTimer;
+    private float introSong;
+    private float gameSong;
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        } else {
+            Debug.Log("Warning: multiple " + this + " in scene!!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        Song = Resources.Load<AudioClip>("Song");
+        Caja = Resources.Load<AudioClip>("Caja");
+        Iman = Resources.Load<AudioClip>("Iman");
+
+
+        introSong = Song.length;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audiosrc.isPlaying)
+        {
+            currentScene = SceneManager.GetActiveScene();
+            sceneName = currentScene.name;
+            if ((sceneName == "MainMenu" || sceneName == "GameOver" || sceneName == "Win"))
+            {
+                PlaySound("Song");
+            }
+            //else
+            //{
+            //    PlaySound("SongGame");
+            //}
+        }
+
+        audiosrc.volume = musicVolume;
+
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+
+    //public static void StopSound() { audiosrc.Stop(); }
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "Song":
+                audiosrc.PlayOneShot(Song);
+                break;
+        }
+    }
+}
